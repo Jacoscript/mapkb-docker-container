6362afa184311b51a00dcb3ba9b45f5c4bf4a68f
Bourquin, Jacques (Contractor) Jules <jbourquin@contractor.usgs.gov>
2020-04-21 17:02:59 -0500

    Added new datastores

--- makb-tomcat/Dockerbuild/docker-entrypoint.sh
+++ makb-tomcat/Dockerbuild/docker-entrypoint.sh
@@ -39,10 +39,11 @@ if [ "$1" = 'catalina.sh' ]; then
         #genonames
         #gnis
         $CURL -X POST -H "Content-Type: application/json" -d '{ "dataStore": { "name": "NHD", "description": "NHD datastore", "connectionParameters": { "entry": [ {"@key":"url","$":"file:///makb_assets/data/nhd/Shape"} ] } } }' $REST/workspaces/usgsns/datastores
-        $CURL -X POST -H "Content-Type: application/json" -d '{ "dataStore": { "name": "NHDPlus", "description": "NHD datastore", "connectionParameters": { "entry": [ {"@key":"url","$":"file:///makb_assets/data/nhdpl/Shape"} ] } } }' $REST/workspaces/usgsns/datastores
+        $CURL -X POST -H "Content-Type: application/json" -d '{ "dataStore": { "name": "NHDPlus", "description": "NHDPlus datastore", "connectionParameters": { "entry": [ {"@key":"url","$":"file:///makb_assets/data/nhdpl/Shape"} ] } } }' $REST/workspaces/usgsns/datastores
         $CURL -X POST -H "Content-Type: application/json" -d '{ "dataStore": { "name": "PADUS_DC", "description": "PADUS datastore", "connectionParameters": { "entry": [ {"@key":"url","$":"file:///makb_assets/data/padus"} ] } } }' $REST/workspaces/usgsns/datastores
         $CURL -X POST -H "Content-Type: application/json" -d '{ "dataStore": { "name": "structures_DC", "description": "Structures datastore for DC", "connectionParameters": { "entry": [ {"@key":"url","$":"file:///makb_assets/data/structures"} ] } } }' $REST/workspaces/usgsns/datastores
         $CURL -X POST -H "Content-Type: application/json" -d '{ "dataStore": { "name": "transportation", "description": "Transportation datastore", "connectionParameters": { "entry": [ {"@key":"url","$":"file:///makb_assets/data/transportation/Shape"} ] } } }' $REST/workspaces/usgsns/datastores
+        $CURL -X POST -H "Content-Type: application/json" -d '{ "dataStore": { "name": "gnis", "description": "GNIS datastore", "connectionParameters": { "entry": [ {"@key":"url","$":"file:///makb_assets/data/DC_Features"} ] } } }' $REST/workspaces/usgsns/datastores
         for i in `ls /makb_assets/json_data`
         do
             #STORE=`cat /makb_assets/json_data/$i | grep -P -o '"dataStore","name":"usgsns:(.*?)"' | grep -P -o '"usgsns:(.*?)"' | sed 's/usgsns://g'`
