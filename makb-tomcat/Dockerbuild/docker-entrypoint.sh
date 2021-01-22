b3978e56c55cfcef98092b7d59348fd613e755d5
Bourquin, Jacques (Contractor) Jules <jbourquin@contractor.usgs.gov>
2021-01-22 12:23:57 +0000

    Editing Docker files to launch Virtuoso

--- makb-tomcat/Dockerbuild/docker-entrypoint.sh
+++ makb-tomcat/Dockerbuild/docker-entrypoint.sh
@@ -5,14 +5,14 @@ SCRIPTS="$MAKB_ASSETS/scripts"
 if [ -e /.is_development ]; then service ssh start; fi
 
 if [ "$1" = 'catalina.sh' ]; then
-    if [ -d /marmotta ]; then
+    if [ -e /.first_run ]; then
         echo "Initializing MAKB, this will take about 60 seconds"
         $1 start
-        cat /usr/local/tomcat/logs/catalina.out | grep "Server startup" > /dev/null
+        cat /opt/tomcat/logs/catalina.out | grep "Server startup" > /dev/null
         while [ $? -ne 0 ]; do
             echo "Waiting for catalina..."
             sleep 1s
-            cat /usr/local/tomcat/logs/catalina.out | grep "Server startup" > /dev/null
+            cat /opt/tomcat/logs/catalina.out | grep "Server startup" > /dev/null
         done
         
         # Configure GeoServer
@@ -66,31 +66,32 @@ if [ "$1" = 'catalina.sh' ]; then
         /makb_assets/scripts/import_marmotta.sh
         $1 stop
         pkill java
-        mv /marmotta/WEB-INF/lib/* /usr/local/tomcat/webapps/marmotta/WEB-INF/lib
-        rm -rf /marmotta/WEB-INF
-        mv /marmotta/config /usr/local/tomcat/webapps/marmotta
-        mv /marmotta/* /usr/local/tomcat/webapps/marmotta
-        rmdir /marmotta
+        # mv /marmotta/WEB-INF/lib/* /usr/local/tomcat/webapps/marmotta/WEB-INF/lib
+        # rm -rf /marmotta/WEB-INF
+        # mv /marmotta/config /usr/local/tomcat/webapps/marmotta
+        # mv /marmotta/* /usr/local/tomcat/webapps/marmotta
+        # rmdir /marmotta
+        rm /.first_run
 
         # Add LD Cache endpoint
-        echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.name = Karma Endpoint - USGS WFS' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
-        echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.prio = 2' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
-        echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.provider = karma rdf service' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
-        echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.pattern = http://data.usgs.gov/*' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
-        echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.service = http://localhost:8080/web-services-rdf-0.0.1-SNAPSHOT/rdf/r2rml/rdf' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
-        echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.expiry = 240' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
-        echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.active = true' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
-        echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.contenttype =' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
+        # echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.name = Karma Endpoint - USGS WFS' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
+        # echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.prio = 2' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
+        # echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.provider = karma rdf service' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
+        # echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.pattern = http://data.usgs.gov/*' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
+        # echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.service = http://localhost:8080/web-services-rdf-0.0.1-SNAPSHOT/rdf/r2rml/rdf' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
+        # echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.expiry = 240' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
+        # echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.active = true' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
+        # echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.contenttype =' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
         
         # Disable security in Marmotta
-        echo 'security.enabled = false' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
+        # echo 'security.enabled = false' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
 
         
 
 
         # Setup database
-        cat /usr/local/tomcat/webapps/marmotta/system-config.properties | sed 's/ = sa/ = postgres/' | sed -e 's/database.url = .*/database.url = jdbc:postgresql:\/\/makb-docker-container_postgresql_1:5432\/marmotta\ndatabase.type = postgres/' > /usr/local/tomcat/webapps/marmotta/system-config.properties.temp
-        mv /usr/local/tomcat/webapps/marmotta/system-config.properties.temp /usr/local/tomcat/webapps/marmotta/system-config.properties
+        #cat /usr/local/tomcat/webapps/marmotta/system-config.properties | sed 's/ = sa/ = postgres/' | sed -e 's/database.url = .*/database.url = jdbc:postgresql:\/\/makb-docker-container_postgresql_1:5432\/marmotta\ndatabase.type = postgres/' > /usr/local/tomcat/webapps/marmotta/system-config.properties.temp
+        # mv /usr/local/tomcat/webapps/marmotta/system-config.properties.temp /usr/local/tomcat/webapps/marmotta/system-config.properties
 
 
         # Clone the repo if we are not in development
