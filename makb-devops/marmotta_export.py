ccc96deaa35a365c65a87b4a3f959e5de4eeb2b9
Bourquin <jbourquin@contractor.usgs.gov>
2021-02-18 14:25:31 -0600

    Created export script for Marmotta

---
+++ makb-devops/marmotta_export.py
@@ -0,0 +1,80 @@
+import pycurl
+import json
+from context import Context
+from typing import Callable, List
+from io import BytesIO
+import pathlib
+
+CONTEXT_URI_PLACEHOLDER: str = "%CONTEXT_URL%"
+EXPORT_URL: str = f"http://144.47.161.52:8080/marmotta/export/download?context={CONTEXT_URI_PLACEHOLDER}" \
+                   "&format=text%2Fturtle"
+GET_CONTEXTS_URL: str = "http://144.47.161.52:8080/marmotta/context/list?labels=true"
+MARMOTTA_CONTEXT_ENCODING = "utf-8"
+EXPORT_FOLDER = "export\\"
+EXT = ".ttl"
+
+
+def get_contexts(c: pycurl.Curl = None, buffer: BytesIO = None) -> List[Context]:
+    if c is None:
+        c = pycurl.Curl()
+    if buffer is None:
+        buffer = BytesIO()
+    contexts: List[Context] = []
+    body = __curl(GET_CONTEXTS_URL, buffer, c)
+    context_dict = json.loads(body)
+    for i in context_dict:
+        contexts.append(Context(**i))
+    return contexts
+
+
+def export_context(context: Context, c: pycurl.Curl = None, buffer: BytesIO = None) -> str:
+    if c is None:
+        c = pycurl.Curl()
+    if buffer is None:
+        buffer = BytesIO()
+    data = __curl(EXPORT_URL.replace(CONTEXT_URI_PLACEHOLDER, context.uri), buffer, c)
+    body: str = data.decode(MARMOTTA_CONTEXT_ENCODING)
+    return body
+
+
+def export_contexts(contexts: List[Context],
+                    save_func: Callable[[Context, str], None] = lambda a, b: None) -> None:
+    for i in contexts:
+        body = export_context(i)
+        save_func(i, body)
+
+
+def save_exported_context(context: Context, body: str) -> None:
+    filename: str = f"{EXPORT_FOLDER}{context.label}{EXT}"
+    f = open(filename, "w")
+    f.write(body)
+    f.close()
+    context.export_file = filename
+
+
+def __curl(url: str, buffer: BytesIO = None, c: pycurl.Curl = None) -> bytes:
+
+    if buffer is None:
+        buffer = BytesIO()
+    if c is None:
+        c = pycurl.Curl()
+
+    c.setopt(c.URL, url)
+    c.setopt(c.WRITEDATA, buffer)
+    c.perform()
+    c.close()
+    return buffer.getvalue()
+
+
+def __create_export_folder():
+    path = pathlib.Path(EXPORT_FOLDER)
+    path.mkdir(parents=True, exist_ok=True)
+
+
+def main():
+    contexts: List[Context] = get_contexts()
+    export_contexts(contexts, save_exported_context)
+
+
+if __name__ == "__main__":
+    main()
