b48649a638e3584ebd7e2717989210f93558e78c
Bourquin, Jacques (Contractor) Jules <jbourquin@contractor.usgs.gov>
2021-03-09 09:03:37 -0600

    Modified filter

--- makb-devops/marmotta_export_test.py
+++ makb-devops/marmotta_export_test.py
@@ -1,10 +1,9 @@
 import marmotta_export as me
+from itertools import filterfalse
 MAX_SIZE = 1000
 contexts = me.get_contexts()
 print(f"Number of contexts: {len(contexts)}")
-for i in contexts:
-    if i.size > MAX_SIZE:
-        contexts.remove(i)
+contexts[:] = filterfalse(lambda t: t.size >= MAX_SIZE, contexts)
 print(f"Number of contexts: {len(contexts)}")
 me.__create_export_folder()
 me.export_contexts(contexts, me.save_exported_context)
