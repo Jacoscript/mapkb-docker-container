373cb44d9afadfb484322887a11d90e228e3da02
Bourquin, Jacques (Contractor) Jules <jbourquin@contractor.usgs.gov>
2021-03-22 15:34:12 -0500

    Writing import script

--- makb-devops/virtuoso_import.py
+++ makb-devops/virtuoso_import.py
@@ -1,4 +1,18 @@
-# TODO: Create import folder to shared as volume mount on /import
-# TODO: Parse file names in /export folder to get list of graph IRIs
+import pathlib
+import sys
+from typing import List
+from typing import Tuple
+WINDOWS: str = "win32"
+IMPORT_FOLDER: str = "import\\" if sys.platform == WINDOWS else "import/"
+GRAPH_URI: str = "$GRAPH_URI"
+QUERY: str = f"ld_dir_all('/export', '${GRAPH_URI}.ttl', 'http://data.usgs.gov/${GRAPH_URI}.ttl');"
+graphs: List[Tuple[str, str]] = []
+
+def __create_import_folder():
+    path = pathlib.Path(IMPORT_FOLDER)
+    path.mkdir(parents=True, exist_ok=True)
+
+
+# TODO: Parse file names in /export folder to get list of graph URIs
 # TODO: Build SQL file to call RDF loader for each ttl file
 # TODO: Export SQL file to be run against isql
