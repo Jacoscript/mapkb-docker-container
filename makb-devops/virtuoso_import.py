ad281a2996876fac4a449c298dd9e0b08469bd58
Bourquin, Jacques (Contractor) Jules <jbourquin@contractor.usgs.gov>
2021-03-22 15:05:06 -0500

    Initial commit of virtuoso_import.py to be used to build import file for Virtuoso

---
+++ makb-devops/virtuoso_import.py
@@ -0,0 +1,4 @@
+# TODO: Create import folder to shared as volume mount on /import
+# TODO: Parse file names in /export folder to get list of graph IRIs
+# TODO: Build SQL file to call RDF loader for each ttl file
+# TODO: Export SQL file to be run against isql
