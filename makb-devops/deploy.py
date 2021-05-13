320f4638ca09ccc329a52b88a3cf206c1061b0b5
Jacques Bourquin <jacques@jacoscript.com>
2021-05-13 16:21:10 -0500

    Working on deployment script

--- makb-devops/deploy.py
+++ makb-devops/deploy.py
@@ -19,11 +19,13 @@ except ImportError:
 
 args = None
 incorrect_arguments = False
-makb_assets_built = False
+# makb_assets_built = False
 PRODUCTION = "production"
 DEVELOPMENT = "development"
 MASTER = "master"
 MAKB_ASSETS = "makb-assets:latest"
+MAKB_VIRTUOSO_CONTAINER = "makb-virtuoso-container"
+MAXIMUM_RETRY_COUNT = 5
 
 
 def _parse_args():
@@ -36,7 +38,7 @@ def _parse_args():
     parser.add_argument("--doi", action="store_true", help="Deploying inside Department of the Interior")
     parser.add_argument("--source_path", action="store", default="",
                         help="Source location for MapKB project in development mode")
-    parser.add_argument("--rebuild_assets", action="store_true", help="Rebuild the makb-assets image, if it exists")
+    parser.add_argument("--reset", action="store_true", help="Remove containers and all images associated with MapKB")
     args = parser.parse_args()
 
 
@@ -44,10 +46,19 @@ def main():
     _parse_args()
     if not _check_for_docker():
         return
-    _check_makb_assets()
+    if args.reset:
+        _reset_all()
+        return
     if args.interactive:
         interactive_mode()
     _build_makb_assets()
+    _build_makb_tomcat()
+    _build_makb_virtuoso()
+    container_name = _deploy_makb_virtuoso()
+    print(f"To stop this container type `docker stop {container_name}`")
+    print(f"To restart it later type `docker start {container_name}`")
+    print(f"To remove it type `docker rm {container_name}` and rerun this script to recreate it")
+    print(f"To reset MapKB and return to a default state type `python deploy.py --reset`")
 
 
 def _argument_is_default(arg_check: Callable[[], bool]):
@@ -94,9 +105,9 @@ def interactive_mode():
                 print("What branch of MapKB would you like to use?  (Leave blank for master)")
                 args.branch = _set_interactive_string()
 
-            if _argument_is_default(lambda: args.rebuild_assets is False) and makb_assets_built:
-                print("MAKB Assets has already been built, would you like to rebuild it?")
-                args.rebuild_assets = _set_interactive_bool()
+            # if _argument_is_default(lambda: args.rebuild_assets is False) and makb_assets_built:
+            #   print("MAKB Assets has already been built, would you like to rebuild it?")
+            #    args.rebuild_assets = _set_interactive_bool()
         _print_interactive_settings()
 
 
@@ -109,16 +120,16 @@ def _print_interactive_settings():
         print(f"Branch: {args.branch}")
     else:
         print(f"MapKB source code location: {args.source_path}")
-    print(f"Rebuild MapKB Assets image: {args.rebuild_assets}")
+    # print(f"Rebuild MapKB Assets image: {args.rebuild_assets}")
 
     print("Are these settings correct?")
     incorrect_arguments = not _set_interactive_bool()
 
 
 def _check_makb_assets() -> None:
-    global makb_assets_built
-    client = docker.DockerClient()
+    client = _create_docker_client()
     images = client.images.list()
+    print("Checking for makb-assets image.")
     for i in images:
         for j in i.tags:
             if j == MAKB_ASSETS:
@@ -130,6 +141,7 @@ def _check_makb_assets() -> None:
 def _build_makb_tomcat() -> None:
     path = "../makb-tomcat/Dockerbuild"
     tag = "makb-tomcat"
+    print("Building makb-tomcat image")
     if args.deployment_mode == DEVELOPMENT:
         buildargs = {
             "ENV": "dev"
@@ -140,46 +152,111 @@ def _build_makb_tomcat() -> None:
 
 
 def _build_makb_assets() -> None:
+    print("Building makb-assets image")
     _build_image("../makb-assets/Dockerbuild", MAKB_ASSETS)
 
 
 def _build_makb_virtuoso() -> None:
     path = "../makb-virtuoso/Dockerbuild"
     tag = "makb-virtuoso"
+    print("Building makb-virtuoso image")
     if args.doi:
         buildargs = {
-            "DOI": "1"
+            "doi": "1"
         }
         _build_image(path, tag, buildargs)
     else:
         _build_image(path, tag)
 
 
-def _deploy_makb_virtuoso() -> None:
-    if args.source_path != "":
-        os.system("docker-compose ../../docker-compose.volume.yml")
+def _build_environment() -> dict:
+    env: dict = {}
+    if args.deployment_mode == DEVELOPMENT:
+        env["ENV"] = "dev"
+    elif args.deployment_mode == PRODUCTION:
+        env["BRANCH"] = args.branch
+
+    return env
+
+
+def _build_volumes() -> dict:
+    volumes: dict = {}
+    if args.deployment_mode == DEVELOPMENT and not _argument_is_default(lambda: args.source_path == ""):
+        volumes[args.source_path] = _create_ro_binding("/var/www/html")
+    return volumes
+
+
+def _build_ports() -> dict:
+    ports = {
+        "8080/tcp": 8080,
+        "8890/tcp": 8890
+    }
+
+    if args.doi:
+        ports["443/tcp"] = 443
     else:
-        os.system("docker-compose ../../docker-compose.yml")
+        ports["80/tcp"] = 80
+
+    if args.deployment_mode == DEVELOPMENT and _argument_is_default(lambda: args.source_path == ""):
+        ports["22/tcp"] = 2222
+    return ports
+
+
+def _create_ro_binding(path: str) -> dict:
+    return {"bind": path, "mode": "ro"}
+
+
+def _deploy_makb_virtuoso() -> str:
+    print("Deploying makb-virtuoso image")
+    client = _create_docker_client()
+    container = client.containers.run("makb-virtuoso", name=MAKB_VIRTUOSO_CONTAINER, volumes=_build_volumes(),
+                                      ports=_build_ports(), detach=True, restart_policy={"Name": "always"})
+    return container.name
 
 
 def _build_image(path: str, tag: str, buildargs: dict = None) -> None:
     if buildargs is None:
         buildargs = {}
-    client = docker.DockerClient()
-    client.images.build(path=path, tag=tag, buildargs=buildargs)
+    client = _create_docker_client()
+    client.images.build(path=path, tag=tag, buildargs=buildargs, rm=True)
+
 
 def _check_for_docker() -> bool:
     result: int
 
-    result = os.system("docker")
+    result = os.system(_fmt_path("docker"))
     if result != 0:
         print("Please install Docker or Docker Desktop", file=stderr)
         return False
-    result = os.system("docker-compose")
-    if result != 0:
-        print("Please install Docker Compose", file=stderr)
-        return False
+    # result = os.system(_fmt_path("docker-compose"))
+    # if result != 0:
+    #    print("Please install Docker Compose", file=stderr)
+    #    return False
     return True
+
+
+def _fmt_path(path: str) -> str:
+    if platform == "win32":
+        path = path.replace("/", "\\")
+    return path
+
+
+def _create_docker_client() -> docker.DockerClient:
+    return docker.from_env()
+
+
+def _reset_all() -> None:
+    tags = ['makb-virtuoso', "makb-tomcat", MAKB_ASSETS]
+    client = _create_docker_client()
+    for i in client.containers.list(all=True):
+        if MAKB_VIRTUOSO_CONTAINER in i.name:
+            print(f"Stopping {i.name}")
+            i.stop()
+            print(f"Removing {i.name}")
+            i.remove()
+
+    for i in tags:
+        client.images.remove(i)
 
 
 if __name__ == "__main__":
---
+++ makb-virtuoso/Dockerbuild/entrypoint.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+MAKB_ASSETS="/makb_assets"
+SCRIPTS="$MAKB_ASSETS/scripts"
+VIRTUOSO_T="/usr/local/virtuoso-opensource/bin/virtuoso-t -f"
+ISQL="/usr/local/virtuoso-opensource/bin/isql localhost dba dba"
+if [ -e /.is_development ]; then service ssh start; fi
+
+# ---  Uncomment all these lines to reenable Tomcat ---
+#if [ "$1" = 'catalina.sh' ]; then
+    if [ -e /.first_run ]; then
+#        echo "Initializing MAKB, this will take about 60 seconds"
+#        $1 start
+#        cat /usr/local/tomcat/logs/catalina.out | grep "Server startup" > /dev/null
+#        while [ $? -ne 0 ]; do
+#            echo "Waiting for catalina..."
+#            sleep 1s
+#            cat /usr/local/tomcat/logs/catalina.out | grep "Server startup" > /dev/null
+#        done
+        
+        # Configure GeoServer
+#        REST='http://localhost:8080/geoserver/rest/'
+#        CURL='curl -u admin:geoserver'
+        
+#        $CURL -X POST -H "Content-Type: application/json" -d '{ "namespace": { "prefix": "usgsns", "uri": "http://data.usgs.gov/" } }' $REST/namespaces
+        #$CURL -X POST -H "Content-Type: application/json" -d '{ "dataStore": { "name": "usgs_prototype_geonames", "description": "shapefiles for the usgs prototype", "connectionParameters": { "entry": [ {"@key":"url","$":"file:///data/shp/geonames"} ] } } }' $REST/workspaces/usgsns/datastores
+        #$CURL -X POST -H "Content-Type: application/json" -d '{ "dataStore": { "name": "usgs_prototype_gnis", "description": "shapefiles for the usgs prototype", "connectionParameters": { "entry": [ {"@key":"url","$":"file:///data/shp/usgs_layers/gnis"} ] } } }' $REST/workspaces/usgsns/datastores
+        #$CURL -X POST -H "Content-Type: application/json" -d '{ "dataStore": { "name": "usgs_prototype_structures", "description": "shapefiles for the usgs prototype", "connectionParameters": { "entry": [ {"@key":"url","$":"file:///data/shp/usgs_layers/structures"} ] } } }' $REST/workspaces/usgsns/datastores
+#        $CURL -X POST -H "Content-Type: application/json" -d '{ "dataStore": { "name": "boundaries", "description": "Boundaries datastore", "connectionParameters": { "entry": [ {"@key":"url","$":"file:///makb_assets/data/boundaries/Shape"} ] } } }' $REST/workspaces/usgsns/datastores
+#        if [ ! -z ${GEOSERVER_URL+x} ]; then 
+#            echo "Running Geoserver layer export"
+#            /makb_assets/scripts/export_geoserver.sh
+#        fi
+#        echo "Running Geoserver layer import"
+#        for i in `ls /makb_assets/json_data`
+#        do
+#            $CURL -X POST -H "Content-Type: application/json"  -d @/makb_assets/json_data/$i $REST/workspaces/usgsns/featuretypes
+            #STORE=`cat /makb_assets/json_data/$i | grep -P -o '"dataStore","name":"usgsns:(.*?)"' | grep -P -o '"usgsns:(.*?)"' | sed 's/usgsns://g'`
+            # TODO Update feature with correct datastore
+#        done
+#        $CURL -X POST -H "Content-Type: application/json" -d '{ "dataStore": { "name": "doi_regions", "description": "DOI Regions datastore", "connectionParameters": { "entry": [ {"@key":"url","$":"file:///makb_assets/data/doi_regions"} ] } } }' $REST/workspaces/usgsns/datastores
+        #genonames
+        #gnis
+#        $CURL -X POST -H "Content-Type: application/json" -d '{ "dataStore": { "name": "NHD", "description": "NHD datastore", "connectionParameters": { "entry": [ {"@key":"url","$":"file:///makb_assets/data/nhd/Shape"} ] } } }' $REST/workspaces/usgsns/datastores
+#        $CURL -X POST -H "Content-Type: application/json" -d '{ "dataStore": { "name": "NHDPlus", "description": "NHDPlus datastore", "connectionParameters": { "entry": [ {"@key":"url","$":"file:///makb_assets/data/nhdpl/Shape"} ] } } }' $REST/workspaces/usgsns/datastores
+#        $CURL -X POST -H "Content-Type: application/json" -d '{ "dataStore": { "name": "PADUS_DC", "description": "PADUS datastore", "connectionParameters": { "entry": [ {"@key":"url","$":"file:///makb_assets/data/padus"} ] } } }' $REST/workspaces/usgsns/datastores
+#        $CURL -X POST -H "Content-Type: application/json" -d '{ "dataStore": { "name": "structures_DC", "description": "Structures datastore for DC", "connectionParameters": { "entry": [ {"@key":"url","$":"file:///makb_assets/data/structures"} ] } } }' $REST/workspaces/usgsns/datastores
+#        $CURL -X POST -H "Content-Type: application/json" -d '{ "dataStore": { "name": "transportation", "description": "Transportation datastore", "connectionParameters": { "entry": [ {"@key":"url","$":"file:///makb_assets/data/transportation/Shape"} ] } } }' $REST/workspaces/usgsns/datastores
+#        $CURL -X POST -H "Content-Type: application/json" -d '{ "dataStore": { "name": "gnis_DC", "description": "GNIS datastore", "connectionParameters": { "entry": [ {"@key":"url","$":"file:///makb_assets/data/DC_Features"} ] } } }' $REST/workspaces/usgsns/datastores
+#        $CURL -X POST -H "Content-Type: application/json" -d '{ "dataStore": { "name": "tester", "description": "Tester datastore", "connectionParameters": { "entry": [ {"@key":"url","$":"file:///makb_assets/data/tester/Shape"} ] } } }' $REST/workspaces/usgsns/datastores
+#        for i in `ls /makb_assets/json_data`
+#        do
+#            #STORE=`cat /makb_assets/json_data/$i | grep -P -o '"dataStore","name":"usgsns:(.*?)"' | grep -P -o '"usgsns:(.*?)"' | sed 's/usgsns://g'`
+#            # TODO Update feature with correct datastore
+#            featuretype=`echo $i | sed "s/\..*//g"`
+#            $CURL -X PUT -H "Content-Type: application/json"  -d @/makb_assets/json_data/$i $REST/workspaces/usgsns/datastores/boundaries/featuretypes/$featuretype
+#        done
+        # If geoserver url is set, run the export script, otherwise just use what is in the json data folder
+
+
+
+#        mkdir /Web-Karma/karma-web-services/web-services-rdf/src/main/webapp/examples; \
+        #for i in `ls /makb_assets/models/`; do mv /makb_assets/models/$i /usr/local/tomcat/webapps/examples; done
+#        for i in `ls /makb_assets/models/`; do mv /makb_assets/models/$i /Web-Karma/karma-web-services/web-services-rdf/src/main/webapp/examples/; done
+#        mvn -Djetty.port=9999 -f /Web-Karma/karma-web-services/web-services-rdf/pom.xml jetty:run > /jetty.out 2>&1 &
+#        cat /jetty.out | grep "Started Jetty Server" > /dev/null
+#        while [ $? -ne 0 ]; do echo "Waiting for Jetty..."; sleep 1s; cat /jetty.out | grep "Started Jetty Server" > /dev/null; done
+#        rm -rf /makb_assets/rdf_data
+#        /makb_assets/scripts/convert.sh
+#        /makb_assets/scripts/import_marmotta.sh
+#        $1 stop
+#        pkill java
+        # mv /marmotta/WEB-INF/lib/* /usr/local/tomcat/webapps/marmotta/WEB-INF/lib
+        # rm -rf /marmotta/WEB-INF
+        # mv /marmotta/config /usr/local/tomcat/webapps/marmotta
+        # mv /marmotta/* /usr/local/tomcat/webapps/marmotta
+        # rmdir /marmotta
+        rm /.first_run
+
+        # This is no longer necessary.
+        # Add LD Cache endpoint
+        # echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.name = Karma Endpoint - USGS WFS' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
+        # echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.prio = 2' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
+        # echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.provider = karma rdf service' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
+        # echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.pattern = http://data.usgs.gov/*' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
+        # echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.service = http://localhost:8080/web-services-rdf-0.0.1-SNAPSHOT/rdf/r2rml/rdf' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
+        # echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.expiry = 240' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
+        # echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.active = true' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
+        # echo 'ldcache.endpoint.karma\ endpoint\ \ usgs\ wfs.contenttype =' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
+        
+        # Disable security in Marmotta
+        # echo 'security.enabled = false' >> /usr/local/tomcat/webapps/marmotta/system-config.properties
+
+        
+
+
+        # Setup database - this is no longer necessary
+        #cat /usr/local/tomcat/webapps/marmotta/system-config.properties | sed 's/ = sa/ = postgres/' | sed -e 's/database.url = .*/database.url = jdbc:postgresql:\/\/makb-docker-container_postgresql_1:5432\/marmotta\ndatabase.type = postgres/' > /usr/local/tomcat/webapps/marmotta/system-config.properties.temp
+        # mv /usr/local/tomcat/webapps/marmotta/system-config.properties.temp /usr/local/tomcat/webapps/marmotta/system-config.properties
+
+
+        # Clone the repo if we are not in development
+        if [ ! -e /.is_development ]; then
+            rm -rf /var/www/html/* 
+            git clone https://code.usgs.gov/makb/MapKB.git /var/www/html/
+            if [ ! -z ${BRANCH+x} ]; then 
+                cd /var/www/html/
+                git checkout $BRANCH 
+            fi
+        fi
+
+        # Configure Virtuoso for the first time
+        7z x -o$MAKB_ASSETS/export $MAKB_ASSETS/export.7z
+        cd /usr/local/virtuoso-opensource/var/lib/virtuoso/db
+        ln -s $MAKB_ASSETS/export /export
+        ln -s $MAKB_ASSETS/import /import
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+        $VIRTUOSO_T &
+        $ISQL /import/import.sql
+        $ISQL <(echo "checkpoint;")
+        $ISQL <(echo "shutdown();")
+    fi 
+
+#fi
+$VIRTUOSO_T &
+exec "$@"
